4efbdaea868f4073f72c8c5008ee836ef749d656
ndtbao1997 <nguyendinhthaibao63@gmail.com>
2023-10-19 19:59:23 +0700

    Thai Bao module 2

--- untitled/src/ss7_abstract_and_interface/exec/interface_colorable/shape/Square.java
+++ untitled/src/ss7_abstract_and_interface/exec/interface_colorable/shape/Square.java
@@ -3,6 +3,7 @@ package ss7_abstract_and_interface.exec.interface_colorable.shape;
 import ss7_abstract_and_interface.exec.interface_colorable.colorable.Colorable;
 
 public class Square extends Rectangle implements Colorable {
+    public Square(){}
     public Square(double side) {
         super(side, side);
     }
