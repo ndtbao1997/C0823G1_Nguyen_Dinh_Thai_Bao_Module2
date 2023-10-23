027c7ea567a5774f6012a9ba7c8669bd11b0066c
ndtbao1997 <nguyendinhthaibao63@gmail.com>
2023-10-23 14:25:21 +0700

    Thai Bao module 2

--- untitled/src/ss12_dsa_stack_quece/exec/decimal_system_to_binary_system/BinarySystem.java
+++ untitled/src/ss12_dsa_stack_quece/exec/decimal_system_to_binary_system/BinarySystem.java
@@ -10,12 +10,14 @@ public class BinarySystem {
         System.out.println("Hãy nhập một số nguyên dương bạn muốn");
         int num = Integer.parseInt(scanner.nextLine());
         int newNum;
-        while (num != 1){
+        while (num > 1){
             binaryStack.push(num%2);
             newNum = (int) (num/2);
             num = newNum;
         }
-        binaryStack.push(num%2);
+        if (num == 1){
+            binaryStack.push(num%2);
+        }
         int sizeStack = binaryStack.size();
         System.out.println("Hệ nhị phân có kết quả là");
         for (int i = 0; i < sizeStack; i++) {
