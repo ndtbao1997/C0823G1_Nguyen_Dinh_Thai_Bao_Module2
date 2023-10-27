4191b83e2f28c93b6001e89b5d4b8e0ee707ac24
ndtbao1997 <nguyendinhthaibao63@gmail.com>
2023-10-27 20:05:59 +0700

    Thai Bao module 2

--- untitled/src/ss15_exception/exec/Triangle.java
+++ untitled/src/ss15_exception/exec/Triangle.java
@@ -18,17 +18,18 @@ public class Triangle {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hãy nhập độ dài cạnh a");
         a = Double.parseDouble(scanner.nextLine());
+        if (a <= 0) throw new IllegalTriangleException("Độ dài không hợp lệ");
         System.out.println("Hãy nhập độ dài cạnh b");
         b = Double.parseDouble(scanner.nextLine());
+        if (b <= 0) throw new IllegalTriangleException("Độ dài không hợp lệ");
         System.out.println("Hãy nhập độ dài cạnh c");
         c = Double.parseDouble(scanner.nextLine());
+        if (c <= 0) throw new IllegalTriangleException("Độ dài không hợp lệ");
         displaySidesTriangle(a, b, c);
     }
 
     public static void displaySidesTriangle(double a, double b, double c) throws Exception {
-        boolean checkSide = (a <= 0) || (b <= 0) || (c <= 0);
         boolean checkSides = (a + b <= c) || (a + c <= b) || (c + b <= a);
-        if (checkSide) throw new IllegalTriangleException("Độ dài cạnh tam giác nhỏ hơn 0");
         if (checkSides) throw new IllegalTriangleException("Độ dài ba cạnh bạn không chính xác");
         System.out.println("Độ dài 3 cạnh của tam giác là : " + a + ", " + b + ", " + c);
     }
