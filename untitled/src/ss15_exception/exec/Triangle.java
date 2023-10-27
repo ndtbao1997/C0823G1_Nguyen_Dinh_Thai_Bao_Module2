d1c08b1f866242a56e9ff32cd19531ee63825736
ndtbao1997 <nguyendinhthaibao63@gmail.com>
2023-10-27 10:40:11 +0700

    Thai Bao module 2

--- untitled/src/ss15_exception/exec/Triangle.java
+++ untitled/src/ss15_exception/exec/Triangle.java
@@ -12,21 +12,21 @@ public class Triangle {
     }
 
     public static void inputSidesTriangle() throws Exception {
-        int a;
-        int b;
-        int c;
+        double a;
+        double b;
+        double c;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hãy nhập độ dài cạnh a");
-        a = Integer.parseInt(scanner.nextLine());
+        a = Double.parseDouble(scanner.nextLine());
         System.out.println("Hãy nhập độ dài cạnh b");
-        b = Integer.parseInt(scanner.nextLine());
+        b = Double.parseDouble(scanner.nextLine());
         System.out.println("Hãy nhập độ dài cạnh c");
-        c = Integer.parseInt(scanner.nextLine());
+        c = Double.parseDouble(scanner.nextLine());
         displaySidesTriangle(a, b, c);
     }
 
-    public static void displaySidesTriangle(int a, int b, int c) throws Exception {
-        boolean checkSide = (a < 1) || (b < 1) || (c < 1);
+    public static void displaySidesTriangle(double a, double b, double c) throws Exception {
+        boolean checkSide = (a <= 0) || (b <= 0) || (c <= 0);
         boolean checkSides = (a + b <= c) || (a + c <= b) || (c + b <= a);
         if (checkSide) throw new IllegalTriangleException("Độ dài cạnh tam giác nhỏ hơn 1");
         if (checkSides) throw new IllegalTriangleException("Độ dài ba cạnh bạn không chính xác");
