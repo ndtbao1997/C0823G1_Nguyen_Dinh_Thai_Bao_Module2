a41920cf0c458bdd16e9e50612969d3add554635
ndtbao1997 <nguyendinhthaibao63@gmail.com>
2023-10-29 13:44:13 +0700

    Thai Bao module 2

--- untitled/src/ss15_exception/exec/Triangle.java
+++ untitled/src/ss15_exception/exec/Triangle.java
@@ -18,19 +18,27 @@ public class Triangle {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hãy nhập độ dài cạnh thứ nhất");
         side1 = Double.parseDouble(scanner.nextLine());
-        if (side1 <= 0) throw new IllegalTriangleException("Độ dài không hợp lệ");
+        if (side1 <= 0) {
+            throw new IllegalTriangleException("Độ dài không hợp lệ");
+        }
         System.out.println("Hãy nhập độ dài cạnh thứ hai");
         side2 = Double.parseDouble(scanner.nextLine());
-        if (side2 <= 0) throw new IllegalTriangleException("Độ dài không hợp lệ");
+        if (side2 <= 0) {
+            throw new IllegalTriangleException("Độ dài không hợp lệ");
+        }
         System.out.println("Hãy nhập độ dài cạnh thứ ba");
         side3 = Double.parseDouble(scanner.nextLine());
-        if (side3 <= 0) throw new IllegalTriangleException("Độ dài không hợp lệ");
+        if (side3 <= 0) {
+            throw new IllegalTriangleException("Độ dài không hợp lệ");
+        }
         displaySidesTriangle(side1, side2, side3);
     }
 
     public static void displaySidesTriangle(double a, double b, double c) throws Exception {
-        boolean checkSides = (a + b <= c) || (a + c <= b) || (c + b <= a);
-        if (checkSides) throw new IllegalTriangleException("Độ dài ba cạnh bạn không chính xác");
+        boolean checkSides = (a + b > c) && (a + c > b) && (c + b > a);
+        if (!checkSides) {
+            throw new IllegalTriangleException("Độ dài ba cạnh bạn không chính xác");
+        }
         System.out.println("Độ dài 3 cạnh của tam giác là : " + a + ", " + b + ", " + c);
     }
 }
