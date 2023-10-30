edae08aa6a8d3d21f1edb5e36ef4319374ff9673
ndtbao1997 <nguyendinhthaibao63@gmail.com>
2023-10-30 10:33:17 +0700

    Thai Bao module 2

--- untitled/src/ss16_io_textfile/exec/copy_file_text/CopyFileText.java
+++ untitled/src/ss16_io_textfile/exec/copy_file_text/CopyFileText.java
@@ -8,47 +8,70 @@ import java.util.List;
 
 public class CopyFileText {
     private static int count = 0;
+
     public static void main(String[] args) {
         CopyFileText copyFileText = new CopyFileText();
         List<String> stringList = copyFileText.readFile(
-                "D:\\C0823G1_Nguyen_Dinh_Thai_Bao_Module1\\module_2\\untitled\\src\\ss16_io_textfile\\" +
-                        "exec\\copy_file_text\\name.txt");
+                "src/ss16_io_textfile/exec/copy_file_text/name.txt");
         copyFileText.writeFile(
-                "D:\\C0823G1_Nguyen_Dinh_Thai_Bao_Module1\\module_2\\untitled\\src\\ss16_io_textfile\\" +
-                        "exec\\copy_file_text\\result.txt", stringList);
+                "src/ss16_io_textfile/exec/copy_file_text/result.txt", stringList);
     }
-    public List<String> readFile(String filePath){
+
+    public List<String> readFile(String filePath) {
         List<String> strings = new ArrayList<>();
-        try{
-            File file = new File(filePath);
-            if (!file.exists()) throw new FileNotFoundException();
-            BufferedReader br = new BufferedReader(new FileReader(file));
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
             String line;
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 strings.add(line);
                 count += line.split("").length;
             }
-            br.close();
-        }catch (Exception e){
-            System.out.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.err.println("Không tìm thấy file");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                br.close();
+            } catch (IOException ioException) {
+                System.err.println(ioException.getMessage());
+            }
+
         }
         return strings;
     }
-    public void writeFile(String filePath, List<String> strings){
-        try{
-            File file = new File(filePath);
-            if (file.isFile()) throw new FileAlreadyExistsException("Tên file đã tồn tại");
-            FileWriter writer = new FileWriter(file);
-            BufferedWriter bufferedWriter = new BufferedWriter(writer);
-            for (int i = 0; i < strings.size(); i++) {
-                if (i == strings.size() -1) bufferedWriter.write(strings.get(i));
-                else bufferedWriter.write(strings.get(i) + "\n");
+
+    public void writeFile(String filePath, List<String> strings) {
+        int totalCharacter;
+        File file;
+        FileWriter writer = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            file = new File(filePath);
+            if (file.isFile()) {
+                throw new FileAlreadyExistsException("Tên file đã tồn tại");
+            }
+            writer = new FileWriter(file);
+            bufferedWriter = new BufferedWriter(writer);
+            for (String string : strings) {
+                bufferedWriter.write(string);
+                bufferedWriter.newLine();
             }
             bufferedWriter.close();
-            System.out.println("Số kí tự trong tệp là: " + count);
-        }
-        catch (IOException e){
+            totalCharacter = count + (strings.size() - 1);
+            System.out.println("Số kí tự trong tệp là: " + totalCharacter);
+        } catch (FileAlreadyExistsException e) {
+            System.err.println("File đã tồn tại");
+        } catch (IOException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                writer.close();
+                bufferedWriter.close();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
         }
     }
 }
--- untitled/src/ss16_io_textfile/exec/read_file_csv/ReadCountry.java
+++ untitled/src/ss16_io_textfile/exec/read_file_csv/ReadCountry.java
@@ -9,25 +9,22 @@ public class ReadCountry {
     private static final List<Country> countryList = new ArrayList<>();
 
     public static void main(String[] args) {
-        readFileCsv("D:\\C0823G1_Nguyen_Dinh_Thai_Bao_Module1\\module_2\\untitled\\" +
-                "src\\ss16_io_textfile\\exec\\read_file_csv\\countries.csv");
+        readFileCsv("src/ss16_io_textfile/exec/read_file_csv/countries.csv");
         for (Country countries : countryList) {
             System.out.println(countries);
         }
     }
 
     public static void readFileCsv(String fileBath) {
-        try {
-            File file = new File(fileBath);
-            if (!file.exists()) throw new FileNotFoundException();
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileBath))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 createCountry(line);
             }
-            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File không tồn tại");
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
     }
 
@@ -38,14 +35,20 @@ public class ReadCountry {
         String name;
         try {
             id = Integer.parseInt(inforCountry[0]);
-            if (id < 1) throw new InformationIsIncorrect("Số id không hợp lệ");
+            if (id < 1) {
+                throw new InformationIsIncorrect("Số id không hợp lệ");
+            }
             code = inforCountry[1];
-            if (Objects.equals(code, "")) throw new InformationIsIncorrect("Code không hợp lệ");
+            if (Objects.equals(code, "")) {
+                throw new InformationIsIncorrect("Code không hợp lệ");
+            }
             name = inforCountry[2];
-            if (Objects.equals(name, "")) throw new InformationIsIncorrect("Tên không hợp lệ");
+            if (Objects.equals(name, "")) {
+                throw new InformationIsIncorrect("Tên không hợp lệ");
+            }
             countryList.add(new Country(id, code, name));
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
     }
 }
